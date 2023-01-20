d3523a8c069901a67ddd9fd32d26ff446108ddda
Claudio Bertini Batista <bertini.b@icloud.com>
2023-01-19 21:18:41 -0300

    :sparkles: feat: Added email template when blocking a user's access

---
+++ resources/views/emails/account/blockAccount.blade.php
@@ -0,0 +1,382 @@
+<html>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>  </title>
+
+  <style type="text/css">
+    /* Take care of image borders and formatting, client hacks */
+    img {
+      max-width: 600px;
+      outline: none;
+      text-decoration: none;
+      -ms-interpolation-mode: bicubic;
+    }
+
+    a img {
+      border: none;
+    }
+
+    table {
+      border-collapse: collapse !important;
+    }
+
+    #outlook a {
+      padding: 0;
+    }
+
+    .ReadMsgBody {
+      width: 100%;
+    }
+
+    .ExternalClass {
+      width: 100%;
+    }
+
+    .backgroundTable {
+      margin: 0 auto;
+      padding: 0;
+      width: 100% !important;
+    }
+
+    table td {
+      border-collapse: collapse;
+    }
+
+    .ExternalClass * {
+      line-height: 115%;
+    }
+
+    .container-for-gmail-android {
+      min-width: 600px;
+    }
+
+
+    /* General styling */
+    * {
+      font-family: Helvetica, Arial, sans-serif;
+    }
+
+    body {
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-size-adjust: none;
+      width: 100% !important;
+      margin: 0 !important;
+      height: 100%;
+      color: #676767;
+    }
+
+    td {
+      font-family: Helvetica, Arial, sans-serif;
+      font-size: 14px;
+      color: #777777;
+      text-align: center;
+      line-height: 21px;
+    }
+
+    a {
+      color: #676767;
+      text-decoration: none !important;
+    }
+
+    .pull-left {
+      text-align: left;
+    }
+
+    .pull-right {
+      text-align: right;
+    }
+
+    .header-lg,
+    .header-md,
+    .header-sm {
+      font-size: 18px;
+      font-weight: 700;
+      line-height: normal;
+      padding: 35px 0 0;
+      color: #4d4d4d;
+    }
+
+    .header-md {
+      font-size: 24px;
+    }
+
+    .header-sm {
+      padding: 5px 0;
+      font-size: 18px;
+      line-height: 1.3;
+    }
+
+    .content-padding {
+      padding: 20px 0 30px;
+    }
+
+    .mobile-header-padding-right {
+      width: 290px;
+      text-align: right;
+      padding-left: 10px;
+    }
+
+    .mobile-header-padding-left {
+      width: 290px;
+      text-align: left;
+      padding-left: 10px;
+    }
+
+    .free-text {
+      width: 100% !important;
+      padding: 10px 60px 0px;
+    }
+
+    .block-rounded {
+      border-radius: 5px;
+      border: 1px solid #e5e5e5;
+      vertical-align: top;
+    }
+
+    .button {
+      padding: 30px 0;
+    }
+
+    .info-block {
+      padding: 0 20px;
+      width: 260px;
+    }
+
+    .block-rounded {
+      width: 260px;
+    }
+
+    .info-img {
+      width: 258px;
+      border-radius: 5px 5px 0 0;
+    }
+
+    .force-width-img {
+      width: 480px;
+      height: 1px !important;
+    }
+
+    .force-width-full {
+      width: 600px;
+      height: 1px !important;
+    }
+
+    .force-width-gmail {
+      min-width: 600px;
+      height: 0px !important;
+      line-height: 1px !important;
+      font-size: 1px !important;
+    }
+
+    .button-width {
+      width: 228px;
+    }
+  </style>
+
+  <style type="text/css" media="screen">
+    @import url(http://fonts.googleapis.com/css?family=Oxygen:400,700);
+  </style>
+
+  <style type="text/css" media="screen">
+    @media screen {
+
+      /* Thanks Outlook 2013! */
+      * {
+        font-family: "Oxygen", "Helvetica Neue", "Arial", "sans-serif" !important;
+      }
+    }
+  </style>
+
+  <style type="text/css" media="only screen and (max-width: 480px)">
+    /* Mobile styles */
+    @media only screen and (max-width: 480px) {
+
+      table[class*="container-for-gmail-android"] {
+        min-width: 290px !important;
+        width: 100% !important;
+      }
+
+      table[class="w320"] {
+        width: 320px !important;
+      }
+
+      img[class="force-width-gmail"] {
+        display: none !important;
+        width: 0 !important;
+        height: 0 !important;
+      }
+
+
+      a[class="button-width"],
+      a[class="button-mobile"] {
+        width: 248px !important;
+      }
+
+      td[class*="mobile-header-padding-left"] {
+        width: 160px !important;
+        padding-left: 0 !important;
+      }
+
+      td[class*="mobile-header-padding-right"] {
+        width: 160px !important;
+        padding-right: 0 !important;
+      }
+
+      td[class="header-lg"] {
+        font-size: 24px !important;
+        padding-bottom: 5px !important;
+      }
+
+      td[class="header-md"] {
+        font-size: 18px !important;
+        padding-bottom: 5px !important;
+      }
+
+      td[class="content-padding"] {
+        padding: 5px 0 30px !important;
+      }
+
+      td[class="button"] {
+        padding: 5px !important;
+      }
+
+      td[class*="free-text"] {
+        padding: 10px 18px 30px !important;
+      }
+
+      img[class="force-width-img"],
+      img[class="force-width-full"] {
+        display: none !important;
+      }
+
+      td[class="info-block"] {
+        display: block !important;
+        width: 280px !important;
+        padding-bottom: 40px !important;
+      }
+
+      td[class="info-img"],
+      img[class="info-img"] {
+        width: 278px !important;
+      }
+    }
+  </style>
+</head>
+
+<body bgcolor="#f7f7f7">
+  <table align="center" cellpadding="0" cellspacing="0" class="container-for-gmail-android" width="100%">
+    <tr>
+      <td align="left" valign="top" width="100%" style="background:repeat-x url() #ffffff;">
+        <center>
+          <table cellspacing="0" cellpadding="0" width="80%" bgcolor="#ffffff" background="" style="background-color:transparent">
+            <tr>
+              <td width="100%" height="40" valign="top" style="text-align: center; vertical-align:middle;">
+                <center>
+                  <table cellpadding="0" cellspacing="0" width="600" class="w320">
+                    <tr>
+                      <td class="pull-left mobile-header-padding-left" style="vertical-align: middle; padding-top: 18px;">
+                        <center>
+                          <!-- Email logo example -->
+                          <a href="{{env('APP_FRONTEND_URL')}}"><img src="https://dynamic.brandcrowd.com/asset/logo/b802d2e1-c9d0-4b76-8fc5-1b2f5c9ec607/logo-search-grid-1x?v=638003955510630000" class="logo" alt="example-logo" style="display: block; width: 200px; margin-left: auto;  margin-right: auto; margin-bottom: -250px;"></a>
+                        </center>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td height="20px;">
+                      </td>
+                    </tr>
+                  </table>
+                </center>
+                <!--[if gte mso 9]>
+              </v:textbox>
+            </v:rect>
+            <![endif]-->
+              </td>
+            </tr>
+          </table>
+        </center>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <center>
+          <table style="background-color: {!! $bar_color !!}; color: white; width:100%;">
+            <td>
+              <th style="font-size:26px; padding: 20px; font-weight: 400;">
+                {{ env('APP_NAME') }}
+              </th>
+            </td>
+          </table>
+        </center>
+      </td>
+    </tr>
+    <td align="center" valign="top" width="100%" style="background-color: #ffffff;  border-top: 1px solid #e5e5e5; border-bottom: 1px solid #e5e5e5;">
+      <center>
+        <table cellpadding="0" cellspacing="0" width="600" class="w320">
+          <tr>
+            <td class="content-padding">
+              <table cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                  <td class="header-md">
+                    <!--  -->
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td style="padding-bottom: 75px;">
+              <table cellpadding="0" cellspacing="0" width="100%" style="border-collapse:separate !important;">
+                <tr>
+                  <td class="info-block">
+                    <table cellpadding="0" cellspacing="0" width="100%" style="border-collapse:separate !important;">
+                      <tr>
+                        <td class="block-rounded">
+                          <table cellpadding="0" cellspacing="0" width="100%">
+                            <tr>
+                              <td style="padding: 15px;">
+                                <table cellspacing="0" cellpadding="0" width="100%">
+                                  <tr>
+                                    <td style="text-align:left; width:155px; color: {!! $color !!}; background-color: {!! $background_color !!}; padding: 20px; text-align: justify;">
+                                      <h2 style='color: #510A79;'><b><center>Acesso Bloqueado!</center></b></h2><br>
+                                        O seu acesso foi bloqueado devido a várias tentativas de login incorreto.<br><br>
+                                        Por favor, utilize o seu email para recuperar o acesso do sistema <b>{{ env('APP_NAME') }}</b>:<br><br>
+                                      <b>Email:</b> {{ $email }} <br><br>
+                                      <a style='color: #510A79; font-weight: 700;' href='{!! env("APP_FRONTEND_URL") !!}/recovery'><center>Recupere aqui!</center></a><br><br>
+                                      <i>Esta é uma mensagem automática, não responda este e-mail.</i>
+                                    </td>
+                                  </tr>
+                                </table>
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </center>
+    </td>
+    <tr>
+      <td align="center" valign="top" width="100%" style="background-color: {!! $bar_color !!}; height: 50px;">
+        <center>
+          <table cellspacing="0" cellpadding="0" width="600" class="w320">
+            <tr>
+              <td style="padding: 20px 0 20px; font-size:20px;">
+                <a href="https://www.example.com.br" style="font-weight: 300; color: white">Example</a>
+              </td>
+            </tr>
+          </table>
+        </center>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
