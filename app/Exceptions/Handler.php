c510f5312dece593a00cefb30a418793d5b4b916
Claudio Bertini Batista <bertini.b@icloud.com>
2023-01-19 21:17:07 -0300

    :zap: perf: Configured some exception callbacks

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,31 +2,32 @@
 
 namespace App\Exceptions;
 
+use ErrorException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Support\Str;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
-        //
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Auth\Access\AuthorizationException::class,
+        \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+        \Illuminate\Session\TokenMismatchException::class,
+        \Illuminate\Validation\ValidationException::class,
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array<int, string>
      */
@@ -46,5 +47,19 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if(Str::contains($request->url(), '/api'))
+        {
+            if($e instanceof ErrorException) return response()->apiResponse($e);
+
+            elseif ($e instanceof NotFoundHttpException) return response()->apiResponse($e, [], 'Not Found', 404);
+
+            elseif ($e instanceof ThrottleRequestsException) return response()->apiResponse($e, [], 'Meg', 404);
+        }
+
+        return parent::render($request, $e);
     }
 }
