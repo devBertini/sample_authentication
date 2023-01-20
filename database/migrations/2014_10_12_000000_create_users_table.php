a327bd65cc1a038c0e33a071a06ec11bd3d202d5
Claudio Bertini Batista <bertini.b@icloud.com>
2023-01-19 21:19:23 -0300

    :sparkles: feat: Added new user data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 255);
+            $table->string('username', 30);
+            $table->string('email', 255)->unique();
+            $table->string('mobile', 11)->unique();
+            $table->tinyInteger('admin')->default(false);
+            $table->tinyInteger('active')->default(true);
+            $table->tinyInteger('block')->default(false);
+            $table->tinyInteger('attempts')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
