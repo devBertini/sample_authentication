7ff744ad03626bf3bf6e8ec9550e5378a9a4cc75
Claudio Bertini Batista <bertini.b@icloud.com>
2023-01-20 22:45:13 -0300

    :sparkles: feat: New Unused Route

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Unused Route
+Route::get('/', function () {
+    return response()->json(['success' => 'Unused Route.']);
+});
+
 // Auth
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/recovery', [UserController::class, 'recovey']);
